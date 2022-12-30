6a2955f837e6d4935b8f76b6b98ae78a91aced0a
dev_dwarf <logan.c.forman@gmail.com>
2022-12-29 20:48:13 -0700

    not needed anymore

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-@REM I push a subtree of the repo to github so that it deploys with the deploy folder as root.
-@echo off
-site.exe
-git remote set-url origin git@github.com:dev-dwarf/dev-dwarf.github.io.git
-git subtree push --prefix=deploy origin gh-pages
